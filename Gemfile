7880a7eac4161a746ceeeb5c3eb946485faa583f
Dan <dan.talpau@gmail.com>
2011-04-27 22:09:53 +0100

    determine location based on the IP

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 gem 'sinatra', '1.2.3'
 gem 'thin'
+gem 'json'
--- ola_mundo.rb
+++ ola_mundo.rb
@@ -1,9 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'bundler/setup'
-
+require 'net/http'
+require "uri"
+require 'json'
 
 get '/' do
-  req = Rack::Request.new(env)
-  req.inspect
+#  req = Rack::Request.new(env)
+#  req.inspect
+  ip = request.ip == '127.0.0.1' ? '' : request.ip
+  response = Net::HTTP.get_response URI.parse("http://freegeoip.net/json/#{ip}")
+  result = JSON.parse response.body
+  puts result.inspect
+  "Hello from #{result['country_name']}, #{result['city']}"
 end	
