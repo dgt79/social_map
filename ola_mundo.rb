37f63fe9aaf6533de1ca3a5d1d634dd1040a4455
Dan <dan.talpau@gmail.com>
2011-04-29 00:18:13 +0100

    add a bit of google maps support

--- ola_mundo.rb
+++ ola_mundo.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 require 'net/http'
 require "uri"
 require 'json'
+require 'erb'
 
 get '/' do
 #  req = Rack::Request.new(env)
@@ -12,5 +13,9 @@ get '/' do
   response = Net::HTTP.get_response URI.parse("http://freegeoip.net/json/#{ip}")
   result = JSON.parse response.body
   puts result.inspect
-  "Hello from #{result['country_name']}, #{result['city']}"
-end	
+#  "Hello from #{result['country_name']}, #{result['city']}"
+  
+  @latitude = result['latitude']
+  @longitude = result['longitude']
+  erb :index
+end
